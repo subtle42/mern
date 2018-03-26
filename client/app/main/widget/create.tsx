9106f021e500e9ee8bd64ed90642f94e911f1d22
SINC\daniel.gottlieb <daniel.gottlieb@sseusa.com>
2018-03-26 19:14:28 -0400

    Adding code for create widget in content modal

---
+++ client/app/main/widget/create.tsx
@@ -0,0 +1,146 @@
+import * as React from "react";
+import {Button, Glyphicon, Row, Col} from "react-bootstrap";
+import {ChartType, ColumnType} from "myConstants"
+import WidgetActions from "../../../data/widgets/actions"
+
+interface Props {
+    sourceId?:any;
+}
+
+class State {
+    selected:ChartConf;
+}
+
+class ChartConf {
+    name:string;
+    type:ChartType;
+    requires:ChartConfReq[]
+}
+
+class ChartConfReq {
+    count: number;
+    colType:ColumnType
+}
+
+const chartConfList:ChartConf[] = [{
+    name: "Histogram",
+    type: "histogram",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }]
+}, {
+    name: "Histogram",
+    type: "histogram",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }]
+}, {
+    name: "Histogram",
+    type: "histogram",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }]
+}, {
+    name: "Histogram",
+    type: "histogram",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }]
+}, {
+    name: "Histogram",
+    type: "histogram",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }]
+}, {
+    name: "Histogram",
+    type: "histogram",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }]
+}, {
+    name: "Histogram",
+    type: "histogram",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }]
+}, {
+    name: "Histogram",
+    type: "histogram",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }]
+}, {
+    name: "Pie",
+    type: "pie",
+    requires: [{
+        count: 1,
+        colType: "number"
+    }, {
+        count: 1,
+        colType: "group"
+    }]
+}]
+
+
+export class CreateWidget extends React.Component<Props, State> {
+    state = new State();
+    rowSize = 4;
+    
+
+    done() {
+        let config = {
+            sourceId: this.props.sourceId,
+            type: this.state.selected.type
+        }
+        WidgetActions.create()
+    }
+
+    selectConfig(item) {
+        this.setState({
+            selected:item
+        })
+    }
+
+    buildRows():JSX.Element[] {
+        const availableCharts = chartConfList.filter(x => true);
+        let rows:ChartConf[][] = [];
+        let position = -1;
+        availableCharts.forEach((item, index) => {
+            if (index % this.rowSize === 0) {
+                position++;
+                rows[position] = [];
+            }
+            rows[position].push(item);
+        });
+
+
+        return rows.map(row => {
+            return (<Row>
+            {row.map(col => {
+                return (<Col xs={12/this.rowSize}><Button
+                    onClick={() => this.selectConfig(col)}
+                    bsStyle={col === this.state.selected ? "primary": "warning"}
+                    bsSize="large">
+                        {col.name}
+                    </Button>
+                </Col>)
+            })}
+            </Row>);
+        });
+    }
+
+    render() {
+        return (
+            <div>{this.buildRows()}</div>
+        );
+    }
+}
