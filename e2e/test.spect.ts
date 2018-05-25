51c7e082c9d1743679516a80ee54e476e6efcf39
Daniel Gottlieb <danielgottlieb@Daniels-MacBook-Pro.local>
2018-05-25 10:41:13 -0400

    adding chai to webdriverio

--- e2e/test.spect.ts
+++ e2e/test.spect.ts
@@ -1,10 +1,15 @@
 import {expect} from "chai";
+import * as chai from 'chai';
+import {default as chaiWebdriver} from "chai-webdriverio"
+chai.use(chaiWebdriver(browser));
 
 describe("my first test", () => {
     it("should do a thing", () => {
         let asdf:string = "hello";
         browser.url('http://webdriver.io');
         console.log(browser.getTitle())
+        
         expect(browser.getTitle().length).to.greaterThan(1)
+        expect(browser.getTitle()).to.not.contain("awlfkjawf")
     })
 })
