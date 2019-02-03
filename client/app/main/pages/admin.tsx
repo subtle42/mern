d555661b3477db75428ebee632074a14e4bc0a38
Yusuf Ezzat <yusuf.ezzat@systematicinc.com>
2019-02-02 20:54:37 -0500

    removed list from admin page.

--- client/app/main/pages/admin.tsx
+++ client/app/main/pages/admin.tsx
@@ -4,16 +4,11 @@ import OfferList from '../offers/list'
 import {OfferDetails} from '../offers/details'
 
 export const AdminPage: React.StatelessComponent = () => {
-    return <Container>
+    return (<Container>
         <Row>
             <Col xs={{offset:1, size:10}}>
                 <h1>Admin Page</h1>
             </Col>
         </Row>
-        <Row>
-            <Col xs={{offset:2, size:8}}>
-                <OfferList />
-            </Col>
-        </Row>
-    </Container>
+    </Container>)
 }
