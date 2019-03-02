72b5cd59298cb8ef6de0de666e0b17709859912d
danielcomputer <daniel@Daniel-Gottliebs-MacBook-Pro.local>
2019-03-02 11:12:23 -0500

    got state working with forms

--- client/app/logon/login.tsx
+++ client/app/logon/login.tsx
@@ -7,97 +7,79 @@ import { FormCtrlGroup, FormControl } from '../_common/validation'
 import * as Validators from '../_common/validators'
 import * as utils from '../_common/utils'
 
-class State {
-    rules: FormCtrlGroup
-    loginSuccess?: boolean = false
-}
-
 interface Props {}
 
-export class LoginPage extends React.Component<Props, State> {
-    state: State = new State()
+export const LoginPage: React.StatelessComponent<Props> = (props: Props) => {
+    const [loginSuccess, setLoginSuccess] = React.useState(false)
+    const [rules, setRules] = React.useState(new FormCtrlGroup({
+        email: new FormControl('', [
+            Validators.isRequired,
+            Validators.maxLength(100)
+        ]),
+        password: new FormControl('', [
+            Validators.isRequired,
+            Validators.maxLength(100)
+        ])
+    }))
 
-    componentWillMount () {
-        const rules = new FormCtrlGroup({
-            email: new FormControl('', [
-                Validators.isRequired,
-                Validators.maxLength(100)
-            ]),
-            password: new FormControl('', [
-                Validators.isRequired,
-                Validators.maxLength(100)
-            ])
-        })
-        this.setState({ rules })
+    const handleChange = (event: React.FormEvent<any>) => {
+        utils.handleChange(event, rules)
+        setRules(Object.create(rules))
     }
 
-    handleChange = (event: React.FormEvent<any>) => {
-        utils.handleChange(event, this.state.rules)
-        this.setState({
-            rules: this.state.rules
-        })
-    }
-
-    tryLogin = () => {
-        const { email, password } = this.state.rules.value
+    const tryLogin = () => {
+        const { email, password } = rules.value
         AuthActions.login(email, password)
-        .then(() => this.setState({
-            ...(new State()),
-            loginSuccess: true
-        }))
+        .then(() => setLoginSuccess(true))
         .catch(err => NotifActions.notify('danger', err))
     }
 
-    render () {
-        if (this.state.loginSuccess) {
-            return (<Redirect to='home' />)
-        }
-
-        return (
-            <Form>
-                <FormGroup><Col xs={{ size: 6, offset: 3 }}>
-                <FormGroup id='formEmail'>
-                    <Col sm={2}>
-                        Email
-                    </Col>
-                    <Col sm={10}>
-                      <Input
-                          type='email'
-                          name='email'
-                          value={this.state.rules.get('email').value}
-                          invalid={this.state.rules.get('email').invalid}
-                          placeholder='Email'
-                          onChange={this.handleChange} />
-                        <FormFeedback>{utils.getError(this.state.rules.get('email'))}</FormFeedback>
-                    </Col>
-                </FormGroup>
-                <FormGroup id='formPassword'>
-                    <Col sm={2}>
-                        Password
-                    </Col>
-                    <Col sm={10}>
-                        <Input
-                            type='text'
-                            value={this.state.rules.get('password').value}
-                            invalid={this.state.rules.get('password').invalid}
-                            name='password'
-                            placeholder='Password'
-                            onChange={this.handleChange} />
-                        <FormFeedback>{utils.getError(this.state.rules.get('password'))}</FormFeedback>
-                    </Col>
-                </FormGroup>
-                <FormGroup>
-                <Col sm={{ size: 10, offset: 2 }}>
-                    <Button disabled={!this.state.rules.valid}
-                        className='pull-right'
-                        type='button'
-                        onClick={() => this.tryLogin()}>
-                        Sign in
-                    </Button>
-                </Col>
-                </FormGroup>
-                </Col></FormGroup>
-            </Form>
-        )
+    if (loginSuccess) {
+        return (<Redirect to='home' />)
     }
+
+    return <Form>
+        <FormGroup><Col xs={{ size: 6, offset: 3 }}>
+        <FormGroup>
+            <Col sm={2}>
+                Email
+            </Col>
+            <Col sm={10}>
+                <Input
+                    type='email'
+                    name='email'
+                    value={rules.get('email').value}
+                    invalid={rules.get('email').invalid}
+                    placeholder='Email'
+                    onChange={handleChange} />
+                <FormFeedback>{utils.getError(rules.get('email'))}</FormFeedback>
+            </Col>
+        </FormGroup>
+        <FormGroup>
+            <Col sm={2}>
+                Password
+            </Col>
+            <Col sm={10}>
+                <Input
+                    type='text'
+                    value={rules.get('password').value}
+                    invalid={rules.get('password').invalid}
+                    name='password'
+                    placeholder='Password'
+                    onChange={handleChange} />
+                <FormFeedback>{utils.getError(rules.get('password'))}</FormFeedback>
+            </Col>
+        </FormGroup>
+        <FormGroup>
+        <Col sm={{ size: 10, offset: 2 }}>
+            <Button disabled={!rules.valid}
+                className='pull-right'
+                type='button'
+                onClick={() => tryLogin()}>
+                Sign in
+            </Button>
+        </Col>
+        </FormGroup>
+        </Col></FormGroup>
+    </Form>
 }
